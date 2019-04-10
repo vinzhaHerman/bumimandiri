a075c7b50d9eaf8cac95e5615b6eddbe98ce5a77
vinzhaHerman <49490678+vinzhaHerman@users.noreply.github.com>
2019-04-11 00:34:39 +0700

    Uploaded assets file

---
+++ assets/js/mainjs.js
@@ -0,0 +1,8 @@
+  function openSlideMenu(){
+   document.getElementById('side-menu').style.width='250px';
+   document.getElementById('main1').style.marginLeft='0px';
+ }
+ function closeSlideMenu(){
+   document.getElementById('side-menu').style.width='0px';
+   document.getElementById('main').style.marginLeft='0px';
+ }
