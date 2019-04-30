3d53d0086a7a459960759b66f6746834bdd8fdfc
vinzhaHerman <49490678+vinzhaHerman@users.noreply.github.com>
2019-04-30 17:32:07 +0700

    Install Template untuk ADMIN
    masih plain

---
+++ application/controllers/Admin.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+	
+	function __construct(){
+		parent::__construct();
+		$this->load->helper('url');
+	}
+
+
+	public function index()
+	{
+		$this->load->view('admin/head');
+		$this->load->view('admin/dashboard');
+		$this->load->view('admin/foot');
+	}
+	
+}
