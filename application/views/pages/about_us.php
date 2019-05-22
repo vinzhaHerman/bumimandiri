802cbac04a2cda8b2fd98b30a31ba818f9ea054c
vinzhaHerman <49490678+vinzhaHerman@users.noreply.github.com>
2019-05-22 21:29:51 +0700

    update about us

--- application/views/pages/about_us.php
+++ application/views/pages/about_us.php
@@ -4,9 +4,9 @@
 		<div class="section">
 			<div class="overlay"></div>
 			<div class="para-container">
-				<h1 class="text">BUMI MANDIRI</h1>
-				<h4 class="text">Learning Center, Agriculture, Outbound & Adventure</h4>
-				<p class="para text">Bumi Mandiri Center (BMC) adalah  pusat pelatihan yang menyatu terpadu dengan kegiatan wisata alam. Terletak di kaki Gunung Gede - Pangrango di kawasan Situgunung Cisaat Sukabumi Jawabarat, BMC dibangun di suatu area yang berlatar bukit dan lembah dengan tiga sungai di dalamnya.</p>
+				<h1 class="text title-right">BUMI MANDIRI</h1>
+				<h4 class="text text-right">Learning Center, Agriculture, Outbound & Adventure</h4>
+				<p class="text para-right">Bumi Mandiri Center (BMC) adalah  pusat pelatihan yang menyatu terpadu dengan kegiatan wisata alam. Terletak di kaki Gunung Gede - Pangrango di kawasan Situgunung Cisaat Sukabumi Jawabarat, BMC dibangun di suatu area yang berlatar bukit dan lembah dengan tiga sungai di dalamnya.</p>
 				<div class="symbols-container">
 					<div class="symbol">
 						<i class="far fa-images round"></i>
@@ -29,8 +29,40 @@
 		</div>
 		<div class="section">
 			<div class="path">
-				<div class="para-container"></div>
+				<div class="para-container-left">
+					<div class="title-left-cont"><h1 class="text title title-left">WHY YOU SHOULD<br><span class="highlight">VISIT US</span></h1></div>
+					<h4 class="text">Learning Center, Agriculture, Outbound & Adventure</h4>
+					<p class="text para-left">Bumi Mandiri Center (BMC) adalah  pusat pelatihan yang menyatu terpadu dengan kegiatan wisata alam. Terletak di kaki Gunung Gede - Pangrango di kawasan Situgunung Cisaat Sukabumi Jawabarat, BMC dibangun di suatu area yang berlatar bukit dan lembah dengan tiga sungai di dalamnya.</p>
+					<button class="fill-button-blue" style="float: right; margin-top: 32px;"> Lihat Gallery</button>
+				</div>
 			</div>
+		</div>
+		<div class="section">
+			<h1 class="title" style="color: #3b5998;">TEMUKAN KAMI</h1>
+			<div class="about-grid-container">
+        	<div class="grid-item">
+            	<div class="grid-item-content">
+                	<ul>
+                    	<li><a href="http://localhost/PBumiMandiri/web/kegiatan">Kegiatan Wisata</a></li>
+                    	<li><a href="http://localhost/PBumiMandiri/web/fasilitas">Fasilitas Penginapan</a></li>
+                    	<li><a href="http://localhost/PBumiMandiri/web/daftarpaket">Daftar Paket</a></li>
+                    	<li><a href="#">Profile Perusahaan</a></li>
+                    	<li><a href="#">Help and Support</a></li>
+
+                	</ul>
+            	</div>
+        	</div>
+        	<div class="grid-item">
+            	<div class="grid-item-content">
+                	<ul>
+                    	<li><a href="#">Visitor Information</a></li>
+                    	<li><a href="#">Getting Here</a></li>
+                    	<li><a href="#">Contact Us</a></li>
+                    	<li><a href="#">FAQ</a></li>
+                    	<li><a href="#">FeedBac</a></li>
+                	</ul>
+            	</div>
+        	</div>
 		</div>
 	</div>
 </div>
