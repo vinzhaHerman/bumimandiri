dc9863f1173faaee3f34b60319905b63843c774b
vinzhaHerman <49490678+vinzhaHerman@users.noreply.github.com>
2019-06-14 21:02:55 +0700

    update dikit

---
+++ application/controllers/Booking.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Booking extends CI_Controller {
+	
+	function __construct(){
+		parent::__construct();
+		$this->load->helper('url');
+	}
+
+
+	public function index()
+	{
+		$this->load->view('template/head');
+		$this->load->view('pages/book');
+		$this->load->view('template/footer');
+	}
+	
+}
---
+++ application/views/pages/book.php
@@ -0,0 +1,9 @@
+<div class="main-wrapper">
+<div class="main-content-wrapper">
+
+    <div class="filler"></div>
+    <div class="section-title">BOOK NOW</div>
+    <div class="list-container">
+    </div>
+</div>
+</div>
