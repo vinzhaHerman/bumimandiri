e0a6f879b72bb96b28eb73cee4d0f253d96a61c3
Azhary Furqon Dinata <azharynata@gmail.com>
2019-06-24 18:21:11 +0700

    syarat dan ketentuan
    isi narasi syarat dan ketentuan

--- application/views/auth/syaratdanketentuan.php
+++ application/views/auth/syaratdanketentuan.php
@@ -17,55 +17,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div class="sub-title"><h2>Syarat dan Ketentuan</h2></div>
     
     <div class="description"><div class="pp">
-<p>Dengan Mendaftar Akun Dibumi Mandiri.Berarti Anda telah setuju dengan Syarat dan Ketentuan Layanan (untuk selanjutnya disebut "Syarat dan Ketentuan") berikut ini. Anda bisa melihat Syarat dan Ketentuan secara lengkap di halaman ini. Segala macam fitur baru yang ada pada Layanan akan terikat dengan Syarat dan Ketentuan ini. Bumi Mandiri memiliki hak untuk melakukan perubahan sewaktu-waktu. Bumi Mandiri menyarankan Anda untuk membaca Syarat dan Ketentuan ini secara periodik untuk melihat perubahan-perubahan yang Bumi Mandiri buat.</p>
+<p>Dengan Mendaftar Akun Dibumi Mandiri.Berarti Anda telah setuju dengan Syarat dan Ketentuan Layanan berikut ini. Anda bisa melihat Syarat dan Ketentuan secara lengkap di halaman ini. Segala macam fitur baru yang ada pada Layanan akan terikat dengan Syarat dan Ketentuan ini. Bumi Mandiri memiliki hak untuk melakukan perubahan sewaktu-waktu. Bumi Mandiri menyarankan Anda untuk membaca Syarat dan Ketentuan ini secara periodik untuk melihat perubahan-perubahan yang Bumi Mandiri buat.</p>
 <h3>Bagian 1 - Akun</h3>
 <ol>
-<li>LOREM.</li>
-<li>LOREM.</li>
-<li>LOREM</li>
-<li>LOREM</li>
-<li>LOREM</li>
+<li>Anda wajib memberikan nama lengkap dan jelas, alamat email yang valid dan informasi lain yang dibutuhkan dalam pendaftaran layanan Bumi Mandiri.</li>
+<li>Anda berkewajiban untuk menjaga kemanan password Anda, Bumi Mandiri tidak akan bertanggung jawab pada kerugian dan kerusakan yang timbul akibat ketidak mampuan Anda dalam menjaga keamanan password Anda.</li>
+<li>Anda tidak diijinkan menggunakan Bumi Mandiri untuk aktifitas ilegal dan melanggar hukum/undang-undang (termasuk undang-undang hak cipta) di wilayah Anda dan/ataupun wilayah hukum Indonesia</li>
+<li>Anda bertanggung jawab atas semua aktivitas dan konten (data, text, foto, gambar, link) yang Anda unggah melalui akun Anda di Bumi Mandiri.</li>
 <li>Anda dilarang mengirimkan segalam macam worm, virus, kode yang bersifat merusak.</li>
 <li>Pelanggaran akan ketentuan ini akan mengakibatkan dihentikannya akun Anda.</li>
 </ol>
 <h3>Bagian II - Syarat Umum</h3>
 <ol>
-<li>Lorem</li>
-<li>Lorem</li>
-<li>Lorem</li>
-<li>Lorem</li>
+<li>Semua calon pelanggan atau pelanggan Bumi Mandiri terikat dengan fitur paket yang dibeli atau fitur yang telah disetujui sesuai yang tertera disini.</li>
+<li>Penambahan  di luar paket akan di kenakan biaya tambahan yang jumlahnya akan di sesuaikan.</li>
+<li>Resiko penggunaan Layanan ini adalah resiko Anda. Layanan ini disediakan seperti apa adanya tanpa jaminan apapun, tersurat ataupun tersirat.</li>
+<li>Bumi Mandiri tidak menjamin Layanan ini tidak akan mengalami gangguan, tepat waktu, aman ataupun bebas dari kesalahan.</li>
+<li>Segala bentuk penyalahgunaan verbal atau tertulis (termasuk ancaman atau ganjaran) pada setiap konsumen Bumi Mandiri, karyawan Bumi Mandiri, anggota atau petugas akan mengakibatkan dihentikannya akun Anda dengan segera.</li>
+<li>Kegagalan Bumi Mandiri dalam menjalankan atau menerapkan segala hak dan ketentuan pada Syarat dan Ketentuan tidak dapat diartikan sebagai diabaikannya hak atau ketentuan tersebut. Syarat dan Ketentuan Layanan merupakan seluruh perjanjian antara Anda dan Klakat dan mengatur penggunaan Layanan oleh Anda, menggantikan semua perjanjian sebelumnya antara Anda dan Bumi Mandiri (termasuk, namun tidak terbatas pada, setiap versi sebelumnya dari Ketentuan Layanan).</li>
 </ol>
-<h3>Bagian III - LOREM</h3>
+<h3>Bagian III - Biaya dan Pembayaran</h3>
 <ol>
-<li>lorem</li>
-<li>Lorem</li>
-<li>Lorem</li>
+<li>Bumi Mandiri akan mengirimkan invoice ke email Anda setelah Anda memilih paket harga dan meminta Anda untuk melakukan pembayaran dengan metode pembayaran yang telah ditentukan oleh Bumi Mandiri.</li>
 </ol>
-<h3>Bagian IV - LOREM</h3>
+<h3>Bagian IV - Perubahan pada Layanan dan Harga.</h3>
 <ol>
-<li>LOREM</li>
-<li>LOREM</li>
-<li>lorem</li>
-</ol>
-<h3>Bagian V - Biaya dan Pembayaran</h3>
-<ol>
-<li>Lorem</li>
-<li>Lorem</li>
-<li>Lorem</li>
-<li>Lorem</li>
-<li>Lorem</li>
-
-</ol>
-<h3>Bagian VI - Pembatalan dan Penghentian Akun</h3>
-<ol>
-<li>Lorem</li>
-
-</ol>
-<h3>Bagian VII - Perubahan pada Layanan dan Harga</h3>
-<ol>
-<li>Bumi Mandiri berhak melakukan perubahan pada harga dan layanan sewaktu-waktu tanpa pemberitahuan sebelumnya</li>
-<li>Perubahan harga akan berlaku pada pemesanan baru</li>
-</ol>
+<li>Bumi Mandiri berhak melakukan perubahan pada harga dan layanan sewaktu-waktu tanpa pemberitahuan sebelumnya.</li>
+<li>Perubahan harga akan berlaku pada order baru.</li>
 </div>
 
    <div class="clearfix">
