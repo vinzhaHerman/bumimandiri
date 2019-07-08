e757463b3ced9db90a3c2c8952fde7e510e42fd9
vinzhaHerman <49490678+vinzhaHerman@users.noreply.github.com>
2019-07-08 12:15:26 +0700

    UPDATE database

--- bumimandiri.sql
+++ bumimandiri.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 05, 2019 at 06:10 AM
+-- Generation Time: Jul 08, 2019 at 07:13 AM
 -- Server version: 10.3.15-MariaDB
 -- PHP Version: 7.3.6
 
@@ -30,6 +30,7 @@ USE `bumimandiri`;
 -- Table structure for table `admins`
 --
 
+DROP TABLE IF EXISTS `admins`;
 CREATE TABLE IF NOT EXISTS `admins` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
@@ -55,6 +56,7 @@ INSERT INTO `admins` (`id`, `username`, `password`, `nama`, `email`, `telp`, `fo
 -- Table structure for table `fasilitas`
 --
 
+DROP TABLE IF EXISTS `fasilitas`;
 CREATE TABLE IF NOT EXISTS `fasilitas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama_fasilitas` varchar(80) NOT NULL,
@@ -69,6 +71,7 @@ CREATE TABLE IF NOT EXISTS `fasilitas` (
 -- Table structure for table `memesan`
 --
 
+DROP TABLE IF EXISTS `memesan`;
 CREATE TABLE IF NOT EXISTS `memesan` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pelanggan_id` int(11) NOT NULL,
@@ -89,6 +92,7 @@ INSERT INTO `memesan` (`id`, `pelanggan_id`, `paket_riwayat_id`) VALUES
 -- Table structure for table `paket_jenis`
 --
 
+DROP TABLE IF EXISTS `paket_jenis`;
 CREATE TABLE IF NOT EXISTS `paket_jenis` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama_paket` varchar(30) NOT NULL,
@@ -111,6 +115,7 @@ INSERT INTO `paket_jenis` (`id`, `nama_paket`, `deskripsi`) VALUES
 -- Table structure for table `paket_program`
 --
 
+DROP TABLE IF EXISTS `paket_program`;
 CREATE TABLE IF NOT EXISTS `paket_program` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama_program` varchar(30) NOT NULL,
@@ -119,6 +124,7 @@ CREATE TABLE IF NOT EXISTS `paket_program` (
   `lama_kegiatan` varchar(20) NOT NULL,
   `jumlah_hari` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
   `paket_jenis_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
@@ -127,19 +133,19 @@ CREATE TABLE IF NOT EXISTS `paket_program` (
 -- Dumping data for table `paket_program`
 --
 
-INSERT INTO `paket_program` (`id`, `nama_program`, `fasilitas`, `konsumsi`, `lama_kegiatan`, `jumlah_hari`, `harga`, `paket_jenis_id`) VALUES
-(1, 'agro & outbound I', 'list fasilitas agro & outbound', 'list konsumsi agro & outbound', '1 hari', 1, 0, 1),
-(2, 'Agro & Outbound II', 'list fasilitas Agro & Outbound', 'list konsumsi Agro & Outbound', '2 hari 1 malam', 2, 0, 1),
-(3, 'Agro & Outbound III', 'list fasilitas Agro & Outbound III', 'list konsumsi Agro & Outbound III', '3 hari 2 malam', 2, 0, 1),
-(4, 'Latihan Dasar Kepemimpinan I', 'list fasilitas Latihan Dasar Kepemimpinan', 'list konsumsi Latihan Dasar Kepemimpinan', '2 hari 1 malam', 2, 0, 1),
-(5, 'Latihan Dasar Kepemimpinan II', 'list fasilitas Latihan Dasar Kepemimpinan', 'list konsumsi Latihan Dasar Kepemimpinan', '3 hari 2 malam', 3, 0, 1),
-(6, 'Ramadhan Camp', 'list Fasilitas Ramadhan Camp', 'list konsumsi Ramadhan Camp', '2 hari 1 malam', 2, 0, 1),
-(7, 'Ramadhan Camp II', 'list fasilitas Ramadhan Camp', 'list konsumsi Ramadhan Camp', '3 hari 2 malam', 3, 0, 1),
-(8, 'Laboratorium Biologi', 'laboratorium', 'laboratorium', '1 hari', 1, 0, 1),
-(9, 'MEETING I', 'fasilitas', 'konsumsi', '2 hari 1 malam', 2, 0, 2),
-(10, 'MEETING II', 'fasilitas meeting', 'fasilitas meeting', '2 hari 1 malam', 2, 0, 2),
-(11, 'MEETING III', 'fasilitas meeting', 'fasilitas meeting', '1 hari', 1, 0, 2),
-(12, 'fun & recreation', 'list fasilitas fun & recreation', 'list konsumsi fun & recreation', '1 hari', 1, 0, 3);
+INSERT INTO `paket_program` (`id`, `nama_program`, `fasilitas`, `konsumsi`, `lama_kegiatan`, `jumlah_hari`, `harga`, `status`, `paket_jenis_id`) VALUES
+(1, 'agro & outbound I', 'list fasilitas agro & outbound', 'list konsumsi agro & outbound', '1 hari', 1, 0, 0, 1),
+(2, 'Agro & Outbound II', 'list fasilitas Agro & Outbound', 'list konsumsi Agro & Outbound', '2 hari 1 malam', 2, 0, 0, 1),
+(3, 'Agro & Outbound III', 'list fasilitas Agro & Outbound III', 'list konsumsi Agro & Outbound III', '3 hari 2 malam', 2, 0, 0, 1),
+(4, 'Latihan Dasar Kepemimpinan I', 'list fasilitas Latihan Dasar Kepemimpinan', 'list konsumsi Latihan Dasar Kepemimpinan', '2 hari 1 malam', 2, 0, 0, 1),
+(5, 'Latihan Dasar Kepemimpinan II', 'list fasilitas Latihan Dasar Kepemimpinan', 'list konsumsi Latihan Dasar Kepemimpinan', '3 hari 2 malam', 3, 0, 0, 1),
+(6, 'Ramadhan Camp', 'list Fasilitas Ramadhan Camp', 'list konsumsi Ramadhan Camp', '2 hari 1 malam', 2, 0, 0, 1),
+(7, 'Ramadhan Camp II', 'list fasilitas Ramadhan Camp', 'list konsumsi Ramadhan Camp', '3 hari 2 malam', 3, 0, 0, 1),
+(8, 'Laboratorium Biologi', 'laboratorium', 'laboratorium', '1 hari', 1, 0, 0, 1),
+(9, 'MEETING I', 'fasilitas', 'konsumsi', '2 hari 1 malam', 2, 0, 0, 2),
+(10, 'MEETING II', 'fasilitas meeting', 'fasilitas meeting', '2 hari 1 malam', 2, 0, 0, 2),
+(11, 'MEETING III', 'fasilitas meeting', 'fasilitas meeting', '1 hari', 1, 0, 0, 2),
+(12, 'fun & recreation', 'list fasilitas fun & recreation', 'list konsumsi fun & recreation', '1 hari', 1, 0, 0, 3);
 
 -- --------------------------------------------------------
 
@@ -147,6 +153,7 @@ INSERT INTO `paket_program` (`id`, `nama_program`, `fasilitas`, `konsumsi`, `lam
 -- Table structure for table `paket_riwayat`
 --
 
+DROP TABLE IF EXISTS `paket_riwayat`;
 CREATE TABLE IF NOT EXISTS `paket_riwayat` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `check_in` date NOT NULL,
@@ -154,7 +161,6 @@ CREATE TABLE IF NOT EXISTS `paket_riwayat` (
   `reservasi_id` int(11) NOT NULL,
   `paket_program_id` int(11) NOT NULL,
   `pelanggan_id` int(11) NOT NULL,
-  `isactive` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
@@ -162,8 +168,8 @@ CREATE TABLE IF NOT EXISTS `paket_riwayat` (
 -- Dumping data for table `paket_riwayat`
 --
 
-INSERT INTO `paket_riwayat` (`id`, `check_in`, `check_out`, `reservasi_id`, `paket_program_id`, `pelanggan_id`, `isactive`) VALUES
-(1, '2019-07-06', '2019-07-08', 1, 3, 1, 1);
+INSERT INTO `paket_riwayat` (`id`, `check_in`, `check_out`, `reservasi_id`, `paket_program_id`, `pelanggan_id`) VALUES
+(1, '2019-07-06', '2019-07-08', 1, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -171,6 +177,7 @@ INSERT INTO `paket_riwayat` (`id`, `check_in`, `check_out`, `reservasi_id`, `pak
 -- Table structure for table `pelanggan`
 --
 
+DROP TABLE IF EXISTS `pelanggan`;
 CREATE TABLE IF NOT EXISTS `pelanggan` (
   `id` int(11) NOT NULL,
   `nama_depan` varchar(80) NOT NULL,
@@ -188,7 +195,7 @@ CREATE TABLE IF NOT EXISTS `pelanggan` (
 
 INSERT INTO `pelanggan` (`id`, `nama_depan`, `nama_belakang`, `email`, `username`, `password`, `tgl_reg`) VALUES
 (1, 'andreas', 'chandra', 'adnreaschan@gmail.com', 'chandra', '123', '2019-06-28'),
-(2, 'mimi', 'nekochan', 'nekomimi@hotmail.com', 'nekomimi', '098765432112345678900987654321234567890987654321', '2019-07-01');
+(2, 'mimi', 'nekochan', 'nekomimi@hotmail.com', 'nekomimi', '1234', '2019-07-01');
 
 -- --------------------------------------------------------
 
@@ -196,6 +203,7 @@ INSERT INTO `pelanggan` (`id`, `nama_depan`, `nama_belakang`, `email`, `username
 -- Table structure for table `post`
 --
 
+DROP TABLE IF EXISTS `post`;
 CREATE TABLE IF NOT EXISTS `post` (
   `id` int(11) NOT NULL,
   `judul_post` varchar(80) NOT NULL,
@@ -212,27 +220,27 @@ CREATE TABLE IF NOT EXISTS `post` (
 -- Table structure for table `reservasi`
 --
 
+DROP TABLE IF EXISTS `reservasi`;
 CREATE TABLE IF NOT EXISTS `reservasi` (
-  `id` int(11) NOT NULL,
-  `kode_reservasi` char(10) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kode_reservasi` char(20) NOT NULL,
   `tgl_masuk` date NOT NULL,
   `tgl_keluar` date NOT NULL,
   `paket_program_id` int(11) NOT NULL,
-  `metode` varchar(15) NOT NULL,
   `pembayaran` varchar(8) NOT NULL,
   `id_pelanggan` int(11) NOT NULL,
   `bukti` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `kode_reservasi` (`kode_reservasi`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `reservasi`
 --
 
-INSERT INTO `reservasi` (`id`, `kode_reservasi`, `tgl_masuk`, `tgl_keluar`, `paket_program_id`, `metode`, `pembayaran`, `id_pelanggan`, `bukti`) VALUES
-(1, 'RS06190001', '2019-07-06', '2019-07-08', 1, 'online', 'LUNAS', 1, 'dark.png'),
-(2, 'RS07190002', '2019-07-09', '2019-07-10', 3, 'online', '', 2, '');
+INSERT INTO `reservasi` (`id`, `kode_reservasi`, `tgl_masuk`, `tgl_keluar`, `paket_program_id`, `pembayaran`, `id_pelanggan`, `bukti`) VALUES
+(1, 'RS06190001', '2019-07-06', '2019-07-08', 1, 'LUNAS', 1, 'dark.png'),
+(2, 'RS07070002', '2019-07-30', '0000-00-00', 1, 'LUNAS', 1, 'light.png');
 
 -- --------------------------------------------------------
 
@@ -240,6 +248,7 @@ INSERT INTO `reservasi` (`id`, `kode_reservasi`, `tgl_masuk`, `tgl_keluar`, `pak
 -- Table structure for table `testimoni`
 --
 
+DROP TABLE IF EXISTS `testimoni`;
 CREATE TABLE IF NOT EXISTS `testimoni` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `summary` varchar(100) NOT NULL,
