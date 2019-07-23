96aa84108ebeb9c06370120b7fe9016ebe18ad90
vinzhaHerman <49490678+vinzhaHerman@users.noreply.github.com>
2019-07-23 23:55:39 +0700

    UPDATE tampilan list program

--- application/views/pages/paket_list.php
+++ application/views/pages/paket_list.php
@@ -34,49 +34,67 @@
   </div>
 </nav>
 
+
+
+
+
 <div class="container">
   <div class="content">
-
     <div class="row align-items-center mx-auto"  style="width: 75vw;">
       <h5 class="text-primary"><i class="fas fa-arrow-left"></i> Kembali</h5>
     </div>
+
+
+
+
 
     <div class="row text-center align-items-center mx-auto">
       <h2 class="align-items-center mx-auto">PILIH PROGRAM</h2>
     </div>
 
-    <div class="row align-items-center mx-auto" >
+
+
+
+
+    <div class="row align-items-center mx-auto">
     <?php foreach ($res->result() as $post): ?>
-        <div class="card mb-3 align-items-center mx-auto" style="width: 75vw;">
-          <form action="<?php  echo base_url("Booking/paket_detail/").$post->id;?>" method="get" enctype="multipart/form-data">
-            <div class="card-header card-header-primary">
-            <input type='text' class="form-control" name="idpaket" value="<?php echo $_GET['idpaket'] ?>" hidden/>
-            <input type='text' class="form-control" name="namapaket" value="<?php echo $_GET['namapaket'] ?>" hidden/>
-            <h3 class="card-title"><b><?php echo $post->nama_program ?></b></h3>
-            <p class="card-category">Lama Kegiatan: <b><?php echo $post->lama_kegiatan ?></b></p>
+        <div class="col-md-12">
+          <div class="card">
+            <form action="<?php  echo base_url("Booking/paket_detail/").$post->id;?>" method="get" enctype="multipart/form-data">
+            <div class="card-header card-header-success">
+              <input type='text' class="form-control" name="idpaket" value="<?php echo $_GET['idpaket'] ?>" hidden/>
+              <input type='text' class="form-control" name="namapaket" value="<?php echo $_GET['namapaket'] ?>" hidden/>
+              <h3 class="card-title"><b><?php echo $post->nama_program ?></b></h3>
+              <p class="card-category">Lama Kegiatan: <b><?php echo $post->lama_kegiatan ?></b></p>
             </div>
-          <div class="card-body">
-            <div class="row">
-              <div class="col-sm-3"><img style="max-width: 250px; height: auto;" src="<?php echo base_url();?>assets/img/people.jpg" alt=""></div>
-              <div class="col-sm-8">
-                <h3 class="card-title text-info"></h3>
-                <p class="card-text"><?php echo $post->fasilitas ?><br>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <input type="text" id="put" class="form-control" name="jumlahorg" value="<?php echo $post->jumlah_hari ?>" readonly="true" hidden="true">
+            <div class="card-body">
+              <div id="typography">
+                <div class="card-title">
+                  
+                </div>
+                <div class="row">
+                  <div class="col-sm-3"><img style="max-width: 235px; height: auto;" src="<?php echo base_url();?>assets/img/people.jpg" alt=""></div>
+                  <div class="col-sm-8">
+                    <h3 class="card-title text-success">Fasilitas:</h3>
+                    <p class="card-text"><?php echo $post->fasilitas ?></p>
+                    <hr>
+                    <h3 class="card-title text-success">Konsumsi:</h3>
+                    <p class="card-text"><?php echo $post->konsumsi ?></p>
+                    <input type="text" id="put" class="form-control" name="jumlahorg" value="<?php echo $post->jumlah_hari ?>" readonly="true" hidden="true">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                <button type="submit" class="btn btn-warning pull-right">Pilih Paket</button>
+                </div>
               </div>
             </div>
-          
-            <button type="submit" class="btn btn-warning pull-right">Pilih Paket</button>
-            <button class="btn btn-secondary pull-right" type="button" data-toggle="collapse" data-target="#collapsedContent1" aria-expanded="false" aria-controls="collapsedContent1">Deail</button>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            <div class="collapse" id="collapsedContent1">
-            <div class="card card-body"> Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-            </div>
-            </div>
-         </div>
-         </form>
+            </form>
+          </div>
         </div>
-        <?php endforeach; ?>
-      </div>
+    <?php endforeach; ?>
+    </div>
   </div>
 </div>
 
