8e47bbaab60f4cb1542706a8e24b42de829e4a03
vinzhaHerman <49490678+vinzhaHerman@users.noreply.github.com>
2019-08-04 22:50:26 +0700

    UPDATE vendor

--- application/views/admin/template/foot.php
+++ application/views/admin/template/foot.php
@@ -11,20 +11,20 @@
 
 
 
-<!--   Core JS Files   -->
-<script src="<?php echo base_url();?>/assets/js/core/popper.min.js" type="text/javascript"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="<?php echo base_url();?>/assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url();?>/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--   Core JS Files   -->
+  <script src="<?php echo base_url();?>/assets/js/core/popper.min.js" type="text/javascript"></script>
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
+  <script src="<?php echo base_url();?>/assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+  <script src="<?php echo base_url();?>/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
   <!--  Plugin for Sweet Alert -->
   <script src="<?php echo base_url();?>/assets/js/plugins/sweetalert2.js"></script>
   <!-- Forms Validations Plugin -->
-  <script src="<?php echo base_url();?>/assets/js/plugins/jquery.validate.min.js"></script>
+  <!-- <script src="/assets/js/plugins/jquery.validate.min.js"></script> -->
   <!--  Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
   <!-- <script src="/assets/js/plugins/jquery.bootstrap-wizard.js"></script> -->
 
   <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
-  <script src="<?php echo base_url();?>/assets/js/plugins/bootstrap-selectpicker.js" ></script>
+  <!-- <script src="/assets/js/plugins/bootstrap-selectpicker.js" ></script> -->
 
   <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
   <!-- <script src="/assets/js/plugins/bootstrap-datetimepicker.min.js"></script> -->
@@ -32,8 +32,8 @@
   <!--  DataTables.net Plugin, full documentation here: https://datatables.net/    -->
   <!-- <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/sc-2.0.0/datatables.min.js"></script> -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/b-1.5.6/b-html5-1.5.6/b-print-1.5.6/r-2.2.2/datatables.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/b-1.5.6/b-html5-1.5.6/b-print-1.5.6/r-2.2.2/datatables.min.js"></script>
 
   
 
@@ -41,26 +41,26 @@
   <!-- <script src="/assets/js/plugins/bootstrap-tagsinput.js"></script> -->
 
   <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
-  <script src="<?php echo base_url();?>/assets/js/plugins/jasny-bootstrap.min.js"></script>
+  <!-- <script src="/assets/js/plugins/jasny-bootstrap.min.js"></script> -->
 
   <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
-  <script src="<?php echo base_url();?>/assets/js/plugins/fullcalendar.min.js"></script>
+  <!-- <script src="/assets/js/plugins/fullcalendar.min.js"></script> -->
 
   <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
   <!-- <script src="/assets/js/plugins/jquery-jvectormap.js"></script> -->
 
   <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="<?php echo base_url();?>/assets/js/plugins/nouislider.min.js" ></script>
+  <!-- <script src="/assets/js/plugins/nouislider.min.js" ></script> -->
 
   <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script> -->
 
   <!-- Library for adding dinamically elements -->
   <script src="<?php echo base_url();?>/assets/js/plugins/arrive.min.js"></script>
 <!--  Google Maps Plugin    -->
 <!-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> -->
 <!-- Chartist JS -->
-<script src="<?php echo base_url();?>/assets/js/plugins/chartist.min.js"></script>
+<!-- <script src="/assets/js/plugins/chartist.min.js"></script> -->
 <!--  Notifications Plugin    -->
 <script src="<?php echo base_url();?>/assets/js/plugins/bootstrap-notify.js"></script>
 <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
--- application/views/admin/template/head.php
+++ application/views/admin/template/head.php
@@ -3,26 +3,25 @@
 <head>
 	<title>Bumi Mandiri</title>
 	<meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" 
 	integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" 
 	crossorigin="anonymous">
 	<!--     Fonts and icons     -->
-  	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  	<!-- CSS Files -->
-  	<link href="<?php echo base_url();?>/assets/css/material-dashboard.css" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"> -->
+  <!-- CSS Files -->
+  <link href="<?php echo base_url();?>/assets/css/material-dashboard.css" rel="stylesheet" />
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.css">
 	<script src="<?php echo base_url();?>/assets/js/core/jquery.min.js" type="text/javascript"></script>
 	<!-- Plugin for the momentJs  -->
  	<script src="<?php echo base_url();?>/assets/js/plugins/moment.min.js"></script>
  	<!-- chart js -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
-    <!--     datetime picker     -->
-    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/b-1.5.6/b-html5-1.5.6/b-print-1.5.6/r-2.2.2/datatables.min.css"/>
+	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script> -->
+  <!--     datetime picker     -->
+  <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/b-1.5.6/b-html5-1.5.6/b-print-1.5.6/r-2.2.2/datatables.min.css"/>
 </head>
 
 <style type="text/css">
