10b496c57ab318c7663ff895026c2ee688d1769b
vinzhaHerman <vinzhaherman@gmail.com>
2020-01-15 15:06:18 +0700

    fix url for login

--- application/views/template/navbar2.php
+++ application/views/template/navbar2.php
@@ -1,6 +1,6 @@
 <div id="navcon" class="navbar-con">
     <div id="nav-icon-open"><span></span><span></span><span></span><span></span></div>
         
-    <a href="" class="cta"><button class="tmb tmb-md tmb-round tmb-flat">Daftar</button></a>
-    <a href="" class="cta"><button class="tmb tmb-md tmb-border tmb-round tmb-flat">Login</button></a>
+    <a href="<?php echo base_url(); ?>register" class="cta"><button class="tmb tmb-md tmb-round tmb-flat">Daftar</button></a>
+    <a href="<?php echo base_url(); ?>login" class="cta"><button class="tmb tmb-md tmb-border tmb-round tmb-flat">Login</button></a>
 </div class="navbar-con">
